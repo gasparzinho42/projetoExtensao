6316212b48b02b15aeeb48a23ed4416ad59ed451
Davidlucas-dev <davidlucasl1l2l3@gmail.com>
2022-09-24 11:03:24 -0300

    my first commit

---
+++ src/components/botaoPrincipal/styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+export const BotaoCustomizado = styled.button`
+
+height: 155px;
+width: 357px;
+border-radius: 5px;
+background-color: #82DDF0;
+border: none;
+margin-left: 20px;
+
+font-size: 38px;
+font-family:karla;
+`
--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Container } from './styles';
+import BotaoPrincipal from '../../components/botaoPrincipal';
+import Paragrafo from '../../components/texto';
 
 function LandingPage() {
   return (
     <Container>
-        Ajudando você a ter as melhores  experiências, e de quebra poupando seu tempo.
+        <Paragrafo text="Ajudando você a ter as melhores  experiências, e de quebra poupando seu tempo."/>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <BotaoPrincipal text="Cadastrar Estabelecimento" />
+          <BotaoPrincipal text="Procurar estabelecimentos" />
+        </div>
+        
+
     </Container>
   )
 }
