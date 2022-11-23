7464f13e2059b491208e50649a84b4990eac525f
jeffersongbsantos <jeffersongbsantos@outlook.com>
2022-11-23 11:13:14 -0300

    Validação de formulario

--- src/pages/formulario/index.jsx
+++ src/pages/formulario/index.jsx
@@ -12,7 +12,28 @@ import { Container } from '../search/styles'
 
 import { CustomBox, FormContainer } from './styles'
 
-function Formulario() {
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+
+  
+
+   const Formulario = () => {
+
+  const validationPost = yup.object().shape({
+    nome: yup.string().required("Obrigatório o preenchimento!").max(150),
+    address: yup.string().required("Obrigatório o preenchimento!"),
+    district: yup.string().required("Obrigatório o preenchimento!")
+   })
+
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+      resolver: yupResolver(validationPost)
+    })
+
+    const addPost = data => console.log(data)
+
   return (
     <Container justifyContent='flex-start'>
       <Box w='58%'>
@@ -21,20 +42,23 @@ function Formulario() {
         />
       </Box>
 
-      <FormContainer>
+      <FormContainer onSubmit={handleSubmit(addPost)}>
         <Row>
-          <Input label='Nome do estabelecimento *' />
+          <Input label='Nome do estabelecimento *' type="text" name="nome" {...register("nome")} />
+          <p  className="error-message">{errors.nome?.message}</p>
         </Row>
         <Row justifyContent='space-between'>
           <CustomBox>
-            <Input label='Endereço *' />
+            <Input label='Endereço *' type="text" name="address" {...register("address")}/>
+            <p  className="error-message">{errors.address?.message}</p>
           </CustomBox>
           <Box w='20%'>
             <Input label='Número' maxLength={4} />
           </Box>
         </Row>
         <Row>
-          <Input label='Bairro *' />
+          <Input label='Bairro *' type="text" name="district" {...register("district")}/>
+          <p  className="error-message">{errors.district?.message}</p>
         </Row>
         <Row>
           <Input label='Telefone do estabelecimento' />
